3974c266ed642233341ea3cf200e1c11324d8229
zafartm <zafar@truemeridian.com>
2022-03-03 04:55:43 +0500

    migratus plugin for DB migrations

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [metosin/compojure-api "1.1.13"]
-                 [org.clojure/tools.logging "1.2.4"]]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [migratus "1.3.6"]
+                 [mysql/mysql-connector-java "8.0.22"]]
 
   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/jul-factory"]
 
@@ -12,6 +14,13 @@
                    :host   "127.0.0.1"
                    :start? true}}
   :uberjar-name "server.jar"
+
+  :migratus {:store :database
+             :migration-dir "migrations"
+             :db {:connection-uri ~(get (System/getenv) "DATABASE_URL")}}
+
+
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
-                   :plugins      [[lein-ring "0.12.5"]]}})
+                   :plugins      [[lein-ring "0.12.5"]
+                                  [migratus-lein "0.7.3"]]}})
 
